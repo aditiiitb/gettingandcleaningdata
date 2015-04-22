3a623dd0c9a833e548192c5abde592b71dc4b004
Aditi Goyal <adig@microsoft.com>
2015-04-22 16:46:25 -0700

    cleaned up first two steps

--- courseproject/run_analysis.R
+++ courseproject/run_analysis.R
@@ -1,3 +1,5 @@
+
+
 rm (list = ls())
 # Useful functions
 # ===================
@@ -22,23 +24,17 @@ for(i in allfiles)
   file1 <- i
   file2 <- gsub("test", "train", i)
   mergedFile <- gsub("test", "merged", i)
-  # mergetwofiles(file1, file2, mergedFile)
+  mergetwofiles(file1, file2, mergedFile)
 }
 
 # Find the set of mean and std features
-file1 <- "getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/features.txt";
-feat <- readLines(file1) ;
-features <- c(feat[grep("mean()",feat)], feat[grep("std()",feat)]);
-mydf <- data.frame(x = features, stringsAsFactors = FALSE)
-reqFeatures <- mydf %>% separate(x, c("relevantRownums" , "Name", "Type", "Axis"))
-reqColumns <- as.numeric(reqFeatures[,1])
+featuresFile <- "getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/features.txt";
+reqFeatures <- read.table(featuresfile, stringsAsFactors = FALSE);
+reqFeatures <- grep("mean()|std()", reqFeatures[,2], ignore.case = TRUE)
 
-# Read in the merged data set as data frame
- con <- file("getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/merged/X_merged.txt");
- X_merged <- readLines(con) ;
- close(con);
- X_merged <- data.frame(measurements = X_merged, stringsAsFactors = FALSE);
- X_merged <- read.table(textConnection(X_merged$measurements))
-
-req_merged <- select(X_merged, reqColumns)
+# Read in the merged data set 
+dataFileLoc <- "getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/merged/X_merged.txt"
+X_merged <- read.table(dataFileLoc, stringsAsFactors = FALSE, nrows = 10299, 
+                       colClasses = classes , comment.char = "")
+X_merged <- X_merged[,reqFeatures]
 
